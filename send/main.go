32f9411956c1082610f8f930aa627e33febac8d0
kensho0101 <ken.kulome417@gmail.com>
2024-03-01 19:21:18 +0900

    anpai

--- send/main.go
+++ send/main.go
@@ -2,23 +2,21 @@ package send
 
 import (
 	"fmt"
+	"formbot/send/send_e"
 	"github.com/bwmarrin/discordgo"
 	"os"
-	"formbot/send/send_e"
 )
 
 const (
-	EnvClientId     = "client_id"
+	EnvClientId = "client_id"
 )
-
 
 func OnMessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	clientId := os.Getenv(EnvClientId)
 	u := m.Author
 	fmt.Printf("%20s %20s(%20s) > %s\n", m.ChannelID, u.Username, u.ID, m.Content)
 	if u.ID != clientId {
-		send.SendMessage(s, m.ChannelID, u.Mention() + "SendMessage")
-		send.SendReply(s, m.ChannelID, "SendReply", m.Reference())
-		fmt.Println("Message:", m.Content)
+		send.SendMessage(s, m.ChannelID, u.Mention()+"ok1")
+		send.SendReply(s, m.ChannelID,"ok2", m.Reference())
 	}
 }
